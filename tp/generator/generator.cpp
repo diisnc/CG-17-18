d3434e7f3e6db5706f60b5164410ce0be467c646
theManInSpace <v.j.r.c1997@hotmail.com>
2018-03-01 19:43:06 +0000

    Vertex coordinates printed in file

--- tp/generator/generator.cpp
+++ tp/generator/generator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>  
 #include <string>
 #include <Windows.h>
 
@@ -11,8 +12,17 @@ int main(int argc, char** argv) {
         std::string form = argv[1];
         std::cout << form << " é argv1." << std::endl;
 
+        // PLANE
         if(form.compare("plane") == 0 && argc == 20) {
             std::cout << "Está a criar um plano." << std::endl;
+            // Create file and his output stream
+            std::ofstream outfile ("plane.3d");
+            // Read inputs and put them in file, line by line
+            for(int i = 1; i < argc; i++) {
+                outfile << argv[i] << std::endl;
+            }
+            outfile.close();
+
             std::cout << "Triângulo 1:" << std::endl;
             std::cout << "A= (" << argv[2] << "," << argv[3] << "," << argv[4] << ")" << std::endl;
             std::cout << "B= (" << argv[5] << "," << argv[6] << "," << argv[7] << ")" << std::endl;
@@ -22,8 +32,17 @@ int main(int argc, char** argv) {
             std::cout << "B= (" << argv[14] << "," << argv[15] << "," << argv[16] << ")" << std::endl;
             std::cout << "C= (" << argv[17] << "," << argv[18] << "," << argv[19] << ")" << std::endl;
         }
+        // BOX
         else if(form.compare("box") == 0 && argc == 26) {
             std::cout << "Está a criar um quadrado." << std::endl;
+            // Create file and his output stream
+            std::ofstream outfile ("box.3d");
+            // Read inputs and put them in file, line by line
+            for(int i = 1; i < argc; i++) {
+                outfile << argv[i] << std::endl;
+            }
+            outfile.close();
+
             std::cout << "A= (" << argv[2] << "," << argv[3] << "," << argv[4] << ")" << std::endl;
             std::cout << "B= (" << argv[5] << "," << argv[6] << "," << argv[7] << ")" << std::endl;
             std::cout << "C= (" << argv[8] << "," << argv[9] << "," << argv[10] << ")" << std::endl;
@@ -33,14 +52,32 @@ int main(int argc, char** argv) {
             std::cout << "G= (" << argv[20] << "," << argv[21] << "," << argv[22] << ")" << std::endl;
             std::cout << "H= (" << argv[23] << "," << argv[24] << "," << argv[25] << ")" << std::endl;
         }
+        // SPHERE
         else if(form.compare("sphere") == 0 && argc == 5) {
             std::cout << "Está a criar uma esfera." << std::endl;
+            // Create file and his output stream
+            std::ofstream outfile ("sphere.3d");
+            // Read inputs and put them in file, line by line
+            for(int i = 1; i < argc; i++) {
+                outfile << argv[i] << std::endl;
+            }
+            outfile.close();
+
             std::cout << "Raio= " << argv[2] << std::endl;
             std::cout << "Slices= " << argv[3] << std::endl;
             std::cout << "Stacks= " << argv[4] << std::endl;
         }
+        // CONE
         else if(form.compare("cone") == 0 && argc == 6) {
             std::cout << "Está a criar um cone." << std::endl;
+            // Create file and his output stream
+            std::ofstream outfile ("cone.3d");
+            // Read inputs and put them in file, line by line
+            for(int i = 1; i < argc; i++) {
+                outfile << argv[i] << std::endl;
+            }
+            outfile.close();
+
             std::cout << "Raio= " << argv[2] << std::endl;
             std::cout << "Altura= " << argv[3] << std::endl;
             std::cout << "Slices= " << argv[4] << std::endl;
