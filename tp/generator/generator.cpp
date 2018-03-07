04a6640fb6f69e7e5fbc39d66887a3eadc85c068
theManInSpace <v.j.r.c1997@hotmail.com>
2018-03-07 19:29:52 +0000

    Main done and added Point class

--- tp/generator/generator.cpp
+++ tp/generator/generator.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <fstream>  
 #include <string>
+#include <vector>
 #include <Windows.h>
+#include "Point.h"
 
 #define _USE_MATH_DEFINES
 #include <math.h>
+
+using std::string;
 
 /*
 
@@ -19,13 +23,13 @@ TODO:
 
 */
 
-
-void plane(float width, string fileName){
+/*
+void plane(float width, string filename){
     FILE *out;
-    fopen_s(&out, fileName.c_str(),"w"); //open to write
+    fopen_s(&out, filename.c_str(),"w"); //open to write (cleans if file already exists or creates it if not)
 
     if( out != NULL ){
-        std::vector<Point> vertices; //Vector to store the vertices
+        std::vector<Point> vertices; // Vector to store the vertices
 
         //T1
         vertices.push_back(Point( width/2, 0.0,-width/2));
@@ -47,25 +51,24 @@ void plane(float width, string fileName){
 }
 
 //box is placed on XZ plane
-void box(float xcoord, float ycoord, float zcoord, int nDivisions, string fileName){
+void box(float xcoord, float ycoord, float zcoord, int nDivisions, string filename){
 
 
 }
 
-void sphere(float radius, int slices, int stacks, string fileName){
+void sphere(float radius, int slices, int stacks, string filename){
 
 
 }
 
-void cone(float radius, float height, int slices, int stacks, string fileName){
+void cone(float radius, float height, int slices, int stacks, string filename){
 
 
 }
 
-//NOTA: CRIAR NA MAIN PARA A PIRAMIDE
-void pyramid(float height, float width, float length, string fileName){
+void pyramid(float height, float width, float length, string filename){
     FILE *out;
-    fopen_s(&out, fileName.c_str(),"w"); //open to write
+    fopen_s(&out, filename.c_str(),"w"); //open to write
 
     if( out != NULL){
         std::vector<Point> vertices; //Vector to store the vertices
@@ -110,115 +113,146 @@ void pyramid(float height, float width, float length, string fileName){
 
 }
 
-void cylinder(float radius, float height, int slices, string fileName){
+void cylinder(float radius, float height, int slices, string filename){
 
 
 }
+*/
 
 
 int main(int argc, char** argv) {
 
     SetConsoleOutputCP(CP_UTF8); // UTF-8 mode for Windows console
 
-    if(argv[1] != NULL) { // Checks if no argv[1] is not empty
+    if(argv[1] != NULL) { // Checks if program receives nothing to process
 
         std::string form = argv[1];
         std::cout << form << " é argv1." << std::endl;
 
         // PLANE
-        if(form.compare("plane") == 0 && argc == 21) {
+        if(form.compare("plane") == 0 && argc == 4) {
             std::cout << "Está a criar um plano." << std::endl;
-            // Create file and his output stream
-            std::ofstream outfile (argv[20]);
-            // Read inputs and put them in file, line by line
-            for(int i = 1; i < argc-1; i++) { // argc-1 because file name is not a vertex coordinate
-                outfile << argv[i] << std::endl;
-            }
-            outfile.close();
 
-            std::cout << "Triângulo 1:" << std::endl;
-            std::cout << "A= (" << argv[2] << "," << argv[3] << "," << argv[4] << ")" << std::endl;
-            std::cout << "B= (" << argv[5] << "," << argv[6] << "," << argv[7] << ")" << std::endl;
-            std::cout << "C= (" << argv[8] << "," << argv[9] << "," << argv[10] << ")" << std::endl;
-            std::cout << "Triângulo 2:" << std::endl;
-            std::cout << "A= (" << argv[11] << "," << argv[12] << "," << argv[13] << ")" << std::endl;
-            std::cout << "B= (" << argv[14] << "," << argv[15] << "," << argv[16] << ")" << std::endl;
-            std::cout << "C= (" << argv[17] << "," << argv[18] << "," << argv[19] << ")" << std::endl;
+            // Prints width and filename just for DEBUG purposes
+            std::cout << "WIDTH: " << argv[2] << std::endl;
+            float width = std::stof(argv[2]);
+            std::cout << width << std::endl;
 
+            std::cout << "FILENAME: " << argv[3] << std::endl;
+
+            // Call triangle criation function
+            // plane(width,argv[3]);
         }
         // BOX
-        else if(form.compare("box") == 0 && argc == 27) {
-            std::cout << "Está a criar um quadrado." << std::endl;
-            // Create file and his output stream
-            std::ofstream outfile (argv[26]);
-            // Read inputs and put them in file, line by line
-            for(int i = 1; i < argc-1; i++) {
-                outfile << argv[i] << std::endl;
-            }
-            outfile.close();
+        else if(form.compare("box") == 0 && argc == 7) {
+            std::cout << "Está a criar um cubo." << std::endl;
+            
+            // Prints Coordinates, Number of Divisions and Filename just for DEBUG purposes
+            std::cout << "POINT: (" << argv[2] << "," << argv[3] << "," << argv[4] << ")" << std::endl;
+            float x = std::stof(argv[2]);
+            std::cout << x << std::endl;
+            float y = std::stof(argv[3]);
+            std::cout << y << std::endl;
+            float z = std::stof(argv[4]);
+            std::cout << z << std::endl;
 
-            std::cout << "A= (" << argv[2] << "," << argv[3] << "," << argv[4] << ")" << std::endl;
-            std::cout << "B= (" << argv[5] << "," << argv[6] << "," << argv[7] << ")" << std::endl;
-            std::cout << "C= (" << argv[8] << "," << argv[9] << "," << argv[10] << ")" << std::endl;
-            std::cout << "D= (" << argv[11] << "," << argv[12] << "," << argv[13] << ")" << std::endl;
-            std::cout << "E= (" << argv[14] << "," << argv[15] << "," << argv[16] << ")" << std::endl;
-            std::cout << "F= (" << argv[17] << "," << argv[18] << "," << argv[19] << ")" << std::endl;
-            std::cout << "G= (" << argv[20] << "," << argv[21] << "," << argv[22] << ")" << std::endl;
-            std::cout << "H= (" << argv[23] << "," << argv[24] << "," << argv[25] << ")" << std::endl;
+            std::cout << "DIVISIONS: "<< argv[5] << std::endl;
+            int divisions = std::stoi(argv[5]);
+            std::cout << divisions << std::endl;
+
+            std::cout << "FILENAME: " << argv[6] << std::endl;
+
+            // Call triangle criation function
+            // box(x,y,z,divisions,argv[6]);
         }
         // SPHERE
         else if(form.compare("sphere") == 0 && argc == 6) {
             std::cout << "Está a criar uma esfera." << std::endl;
-            // Create file and his output stream
-            std::ofstream outfile (argv[5]);
-            // Read inputs and put them in file, line by line
-            for(int i = 1; i < argc-1; i++) {
-                outfile << argv[i] << std::endl;
-            }
-            outfile.close();
+            
+            // Prints Coordinates, Number of Divisions and Filename just for DEBUG purposes
+            std::cout << "RAIO= " << argv[2] << std::endl;
+            float raio = std::stof(argv[2]);
+            std::cout << raio << std::endl;
 
-            std::cout << "Raio= " << argv[2] << std::endl;
-            std::cout << "Slices= " << argv[3] << std::endl;
-            std::cout << "Stacks= " << argv[4] << std::endl;
+            std::cout << "SLICES= " << argv[3] << std::endl;
+            int slices = std::stoi(argv[3]);
+            std::cout << slices << std::endl;
+
+            std::cout << "STACKS= " << argv[4] << std::endl;
+            int stacks = std::stoi(argv[4]);
+            std::cout << stacks << std::endl;
+
+            std::cout << "FILENAME: " << argv[5] << std::endl;
+
+            // Call triangle criation function
+            // sphere(raio,slices,stacks,argv[5]);
         }
         // CONE
         else if(form.compare("cone") == 0 && argc == 7) {
             std::cout << "Está a criar um cone." << std::endl;
-            // Create file and his output stream
-            std::ofstream outfile (argv[6]);
-            // Read inputs and put them in file, line by line
-            for(int i = 1; i < argc-1; i++) {
-                outfile << argv[i] << std::endl;
-            }
-            outfile.close();
 
-            std::cout << "Raio= " << argv[2] << std::endl;
-            std::cout << "Altura= " << argv[3] << std::endl;
-            std::cout << "Slices= " << argv[4] << std::endl;
-            std::cout << "Stacks= " << argv[5] << std::endl;
+            // Prints Coordinates, Number of Divisions and Filename just for DEBUG purposes
+            std::cout << "RAIO= " << argv[2] << std::endl;
+            float raio = std::stof(argv[2]);
+            std::cout << raio << std::endl;
+
+            std::cout << "ALTURA= " << argv[3] << std::endl;
+            float altura = std::stof(argv[3]);
+            std::cout << altura << std::endl;
+
+            std::cout << "SLICES= " << argv[4] << std::endl;
+            int slices = std::stoi(argv[4]);
+            std::cout << slices << std::endl;
+
+            std::cout << "STACKS= " << argv[5] << std::endl;
+            int stacks = std::stoi(argv[5]);
+            std::cout << stacks << std::endl;
+
+            std::cout << "FILENAME: " << argv[6] << std::endl;
+
+            // Call triangle criation function
+            // cone(raio,altura,slices,stacks,argv[6]);
+        }
+        // PYRAMID
+        else if(form.compare("pyramid") == 0 && argc == 6) {
+            std::cout << "Está a criar uma pirâmide." << std::endl;
+
+            // Prints Coordinates, Number of Divisions and Filename just for DEBUG purposes
+            std::cout << "ALTURA= " << argv[2] << std::endl;
+            float altura = std::stof(argv[2]);
+            std::cout << altura << std::endl;
+
+            std::cout << "LARGURA= " << argv[3] << std::endl;
+            float largura = std::stof(argv[3]);
+            std::cout << largura << std::endl;
+
+            std::cout << "COMPRIMENTO= " << argv[4] << std::endl;
+            float comprimento = std::stof(argv[4]);
+            std::cout << comprimento << std::endl;
+
+            std::cout << "FILENAME: " << argv[5] << std::endl;
+            
+            // Call triangle criation function
+            // pyramid(altura,largura,comprimento,argv[5]);
         }
         // CYLINDER
         else if(form.compare("cylinder") == 0 && argc == 6) {
             std::cout << "Está a criar um cilindro." << std::endl;
-            // Create file and his output stream
-            std::ofstream outfile (argv[5]);
-            // Read inputs and put them in file, line by line
-            for(int i = 1; i < argc-1; i++) {
-                outfile << argv[i] << std::endl;
-            }
-            outfile.close();
 
-            std::cout << "Raio= " << argv[2] << std::endl;
+            // Prints Coordinates, Number of Divisions and Filename just for DEBUG purposes
+            std::cout << "RAIO= " << argv[2] << std::endl;
             float raio = std::stof(argv[2]);
             std::cout << raio << std::endl;
-            std::cout << "Altura= " << argv[3] << std::endl;
+            std::cout << "ALTURA= " << argv[3] << std::endl;
             float altura = std::stof(argv[3]);
             std::cout << altura << std::endl;
-            std::cout << "Slices= " << argv[4] << std::endl;
-            float fatias = std::stof(argv[4]);
+            std::cout << "SLICES= " << argv[4] << std::endl;
+            int fatias = std::stoi(argv[4]);
             std::cout << fatias << std::endl;
+            std::cout << "FILENAME: " << argv[5] << std::endl;
             
-            getCylinder(raio,altura,fatias);
+            // Call triangle criation function
+            // cylinder(raio,altura,fatias,argv[5]);
         }
         else {
             std::cout << argv[1] << " não é válido." << std::endl;
@@ -228,51 +262,4 @@ int main(int argc, char** argv) {
 
     return 0;
     
-}
-
-void getCylinder(float radius, float height, int slices) {
-
-	float alpha = (2 * M_PI) / slices;
-	int i = 0;
-
-	for (i = 0; i < slices; i++) {
-
-        // BOTTOM
-        std::cout << 0 << std::endl;
-        std::cout << "Raio= " << argv[2] << std::endl;
-        std::cout << "Raio= " << argv[2] << std::endl;
-        std::cout << "Raio= " << argv[2] << std::endl;
-        std::cout << "Raio= " << argv[2] << std::endl;
-        std::cout << "Raio= " << argv[2] << std::endl;
-		glVertex3f(0, 0, 0);
-		glVertex3f(sin(alpha*(i + 1))*radius,0,cos(alpha*(i+1))*radius);
-		glVertex3f(sin(alpha*(i))*radius, 0, cos(alpha*(i))*radius);
-
-		// TOP
-		glBegin(GL_TRIANGLES);
-		glColor3f(1, 1 - (0.01 * i), 0);
-		glVertex3f(0, height, 0);
-		glVertex3f(sin(alpha*(i))*radius, height, cos(alpha*(i))*radius);
-		glVertex3f(sin(alpha*(i + 1))*radius, height, cos(alpha*(i + 1))*radius);
-		glEnd();
-
-		// MIDDLE FIRST TRIANGLE
-		glBegin(GL_TRIANGLES);
-		glColor3f(1, 1 - (0.01 * i), 0);
-		glVertex3f(sin(alpha*(i))*radius, 0, cos(alpha*(i))*radius);
-		glVertex3f(sin(alpha*(i + 1))*radius, height, cos(alpha*(i + 1))*radius);
-		glVertex3f(sin(alpha*(i))*radius, height, cos(alpha*(i))*radius);
-		glEnd();
-
-		// MIDDLE SECOND TRIANGLE
-		glBegin(GL_TRIANGLES);
-		glColor3f(1 - (0.01 * i), 1 - (0.01 * i), 0);
-		glVertex3f(sin(alpha*(i))*radius, 0, cos(alpha*(i))*radius);
-		glVertex3f(sin(alpha*(i + 1))*radius, 0, cos(alpha*(i + 1))*radius);
-		glVertex3f(sin(alpha*(i + 1))*radius, height, cos(alpha*(i + 1))*radius);
-		glEnd();
-
-
-	}
-
 }
